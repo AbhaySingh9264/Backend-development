208a29db4078c700284c38d5935867620adf5615
abhay <luckeythakur774@gmail.com>
2026-02-02 11:06:49 +0530

    add assignment

---
+++ assignment/q2.js
@@ -0,0 +1,10 @@
+
+const fs=require("fs");
+const timeStamps=new Date(Date.now());
+
+fs.appendFile("text.txt",
+    `date = ${timeStamps}`,(err)=>{
+        if(err){
+            console.log("error in append file");
+        }
+    })
